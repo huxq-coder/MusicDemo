0475a0519ac796fa26ab2d1b15a7471de6080ec3
huxq <huxingqiang@d1ev.com>
2020-06-16 11:24:04 +0800

    工程提交

---
+++ MusicDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MusicDemo
+//
+//  Created by evc_admin on 2020/6/16.
+//  Copyright © 2020 d1ev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ MusicDemo/Class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicDemo
+//
+//  Created by evc_admin on 2020/6/16.
+//  Copyright © 2020 d1ev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
